4ed7ae32b846a7ed0f3652a5c158bd3134652d47
Emmanuelle Jim Arce <emmanuelle.arce@emmanuelle.arce-CKQ1H46KXL>
2022-10-11 09:46:44 +0800

    added file .prettierrc and use prettier command

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const { parse, resolve, toString, toArray } = require('@estilles/expression-parser');
+const {
+  parse,
+  resolve,
+  toString,
+  toArray,
+} = require('@estilles/expression-parser');
 
 let args = process.argv.slice(2);
 
 if (args.length !== 1) {
-    console.log("the number of arugments");
-    process.exit(1);
+  console.log('the number of arugments');
+  process.exit(1);
 }
 
 console.log(toString(parse(args[0])));
